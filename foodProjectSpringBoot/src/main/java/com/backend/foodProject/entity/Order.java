9f4b7ac14ec5f4c7b89aec5df763080dc3257e22
Saw William Joseph <sawwilliamjoseph@gmail.com>
2024-04-20 03:17:58 +0800

    .

--- foodProjectSpringBoot/src/main/java/com/backend/foodProject/entity/Order.java
+++ foodProjectSpringBoot/src/main/java/com/backend/foodProject/entity/Order.java
@@ -92,10 +92,7 @@ public class Order {
     }
 
     public void setTimeOfOrder(Timestamp timeOfOrder) {
-        LocalDateTime localDateTime = timeOfOrder.toLocalDateTime();
-        LocalDateTime adjustedDateTime = localDateTime.minusHours(8);
-        ZonedDateTime zonedDateTime = adjustedDateTime.atZone(ZoneId.of("Asia/Singapore"));
-        this.timeOfOrder = Timestamp.valueOf(zonedDateTime.toLocalDateTime());
+        this.timeOfOrder = timeOfOrder;
     }
 
     public float getTotalBill() {
