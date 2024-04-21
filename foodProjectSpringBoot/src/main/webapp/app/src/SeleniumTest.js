644f6096770485c5870f219bc10d0974be147b1f
Sneakykk <kiakaiphua@yahoo.com.sg>
2024-04-22 00:07:09 +0800

    Editing SeleniumTest.js

--- foodProjectSpringBoot/src/main/webapp/app/src/SeleniumTest.js
+++ foodProjectSpringBoot/src/main/webapp/app/src/SeleniumTest.js
@@ -18,6 +18,11 @@ describe("Login", function () {
       .setChromeOptions(options)
       .build();
 
+    driver2 = await new Builder()
+      .forBrowser("chrome")
+      .setChromeOptions(options)
+      .build();
+
     vars = {};
   });
 
@@ -30,21 +35,20 @@ describe("Login", function () {
   it("Login", async function () {
 
     try{
-      await driver.get("https://152.42.233.119:8443/api/user/login");                  
-      // Click on the "details" button to show the advanced options
-      await driver.findElement(By.id("details-button")).click();                  
-      //Wait for the "Proceed" link to appear and click on it
-      await driver.wait(until.elementLocated(By.id("proceed-link")), 5000);         
-      await driver.findElement(By.id("proceed-link")).click();
       await driver.get("https://octopus-app-m8hyy.ondigitalocean.app");
       await driver.findElement(By.name("username")).click();
       await driver.findElement(By.name("username")).sendKeys("pck");
       await driver.findElement(By.name("password")).click();
       await driver.findElement(By.name("password")).sendKeys("passwordPCK");
       await driver.findElement(By.css(".login-button")).click();
-  
       // Introducing a wait to see what happens before fetching the text
-      await driver.sleep(1000); // Wait for 1 second before fetching the text for debugging
+      await driver.sleep(10000); // Wait for 1 second before fetching the text for debugging
+      await driver2.get("https://152.42.233.119:8443/api/user/login");                  
+      // Click on the "details" button to show the advanced options
+      await driver2.findElement(By.id("details-button")).click();                  
+      //Wait for the "Proceed" link to appear and click on it
+      await driver2.wait(until.elementLocated(By.id("proceed-link")), 5000);         
+      await driver2.findElement(By.id("proceed-link")).click();
       const dashboardText = await driver.findElement(By.css("h2")).getText();
       console.log("Dashboard Text: ", dashboardText); // This will show you what is being captured
       assert.strictEqual(dashboardText, "Dashboard");
