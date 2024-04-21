dfc3c749fab6c1acd7bfafd8a0e3ed8c2eb9115b
Sneakykk <kiakaiphua@yahoo.com.sg>
2024-04-21 23:41:32 +0800

    Fixing SeleniumTest.js

--- foodProjectSpringBoot/src/main/webapp/app/src/SeleniumTest.js
+++ foodProjectSpringBoot/src/main/webapp/app/src/SeleniumTest.js
@@ -28,7 +28,7 @@ describe("Login", function () {
   });
 
   it("Login", async function () {
-    await driver.get("http://localhost:3000/");
+    await driver.get("https://octopus-app-m8hyy.ondigitalocean.app");
     await driver.findElement(By.name("username")).click();
     await driver.findElement(By.name("username")).sendKeys("pck");
     await driver.findElement(By.name("password")).click();
